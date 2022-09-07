135b2fe5cb9e51e4f052ea3189e79aed1d5e97c5
3dw1nM0535 <workockmoses@gmail.com>
2022-09-07 14:51:45 +0300

    fix: logger

--- services/log_service.go
+++ services/log_service.go
@@ -7,12 +7,13 @@ import (
 
 func NewLogger(cfg *nyatta_context.Config) (log *zap.Logger, err error) {
 	var logger *zap.Logger
-	if cfg.Env == "production" || cfg.Env == "test" {
+	if cfg.Env == "development" || cfg.Env == "test" {
 		newLogger, err := zap.NewDevelopment()
 		if err = err; err != nil {
 			return nil, err
 		}
 		logger = newLogger
+		return logger, nil
 	}
 	newLogger, err := zap.NewProduction()
 	if err = err; err != nil {
