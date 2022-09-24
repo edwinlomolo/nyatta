b1b0e97b96ee12cf4759d61fefe26de01bafa5c5
3dw1nM0535 <workockmoses@gmail.com>
2022-09-24 15:48:36 +0300

    chore:test cases

---
+++ api_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	h "github.com/3dw1nM0535/nyatta/handler"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_root_api(t *testing.T) {
+	reqData := "Hello, world"
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, reqData)
+	})
+
+	t.Run("should_drop_unauthed_api_request", func(t *testing.T) {
+		srv := httptest.NewServer(h.Authenticate(handler))
+		defer srv.Close()
+
+		req, err := http.NewRequest(http.MethodPost, srv.URL, nil)
+		assert.Nil(t, err)
+
+		c := srv.Client()
+		res, err := c.Do(req)
+		assert.Nil(t, err)
+		defer res.Body.Close()
+		assert.Equal(t, res.Status, "401 Unauthorized")
+
+		data, err := ioutil.ReadAll(res.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, string(data), "Unauthorized")
+	})
+}
