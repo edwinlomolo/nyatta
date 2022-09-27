b69cb8a9dce22096605092c7657b7dcc0f7aa8a6
3dw1nM0535 <workockmoses@gmail.com>
2022-09-27 16:12:22 +0300

    feat: query property(s) by town/postal_code

--- services/property_service.go
+++ services/property_service.go
@@ -1,23 +1,81 @@
 package services
 
 import (
+	"errors"
+
+	"github.com/3dw1nM0535/nyatta/graph/model"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 type PropertyService interface {
 	ServiceName() string
+	CreateProperty(*model.NewProperty) (*model.Property, error)
+	GetProperty(id string) (*model.Property, error)
+	FindByTown(town string) ([]*model.Property, error)
+	FindByPostalCode(postalCode string) ([]*model.Property, error)
 }
 
 type PropertyServices struct {
-	db     *gorm.DB
+	store  *gorm.DB
 	logger *zap.SugaredLogger
 }
 
 func NewPropertyService(store *gorm.DB, logger *zap.SugaredLogger) *PropertyServices {
-	return &PropertyServices{db: store, logger: logger}
+	return &PropertyServices{store: store, logger: logger}
 }
 
 func (p PropertyServices) ServiceName() string {
 	return "PropertyServices"
+}
+
+func (p *PropertyServices) CreateProperty(property *model.NewProperty) (*model.Property, error) {
+	newProperty := &model.Property{
+		Name:       property.Name,
+		Town:       property.Town,
+		PostalCode: property.PostalCode,
+	}
+
+	err := p.store.Create(&newProperty).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return newProperty, nil
+}
+
+func (p *PropertyServices) GetProperty(id string) (*model.Property, error) {
+	var foundProperty *model.Property
+	err := p.store.Where("id = ?", id).Find(&foundProperty).Error
+	if err != nil {
+		return nil, err
+	}
+
+	if foundProperty.ID != id {
+		return nil, errors.New("Property does not exist")
+	}
+
+	return foundProperty, nil
+}
+
+func (p *PropertyServices) FindByTown(town string) ([]*model.Property, error) {
+	var foundProperties []*model.Property
+
+	err := p.store.Where("town = ?", town).Find(&foundProperties).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return foundProperties, nil
+}
+
+func (p *PropertyServices) FindByPostalCode(postalCode string) ([]*model.Property, error) {
+	var foundProperties []*model.Property
+
+	err := p.store.Where("postal_code = ?", postalCode).Find(&foundProperties).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return foundProperties, nil
 }
