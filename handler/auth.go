f545d5de5643946feebcb4f4ac40304d7af3591c
3dw1nM0535 <workockmoses@gmail.com>
2022-11-14 00:30:57 +0300

    chore: documentations

--- handler/auth.go
+++ handler/auth.go
@@ -24,8 +24,11 @@ func Login() http.Handler {
 		loginResponse := &model.LoginResponse{}
 		var newUser *model.NewUser
 
+		// Read incoming data from body request
 		reqBody, _ := ioutil.ReadAll(r.Body)
 		json.Unmarshal(reqBody, &newUser)
+
+		// SignIn - authorize incoming user
 		token, err := ctx.Value("userService").(*services.UserServices).SignIn(newUser)
 		if err != nil {
 			response := &model.Response{
@@ -54,25 +57,30 @@ func Authenticate(h http.Handler) http.Handler {
 			isAuthorized bool
 		)
 
+		// Authenticate - authenticate incoming request(s)
 		ctx := r.Context()
+		// Validate incoming Authorization header
 		token, err := validateBearerAuthHeader(ctx, r)
 		if err == nil {
 			isAuthorized = true
 			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+				// Grab user_id from jwt claims
 				userIdBytes, _ := base64.StdEncoding.DecodeString(claims["id"].(string))
 				userId = string(userIdBytes[:])
 			}
 		} else {
-			// TODO use logger
-			w.WriteHeader(http.StatusUnauthorized)
+			// Failed authenticating Authorization header
+			w.WriteHeader(http.StatusUnauthorized) // 401:Unauthorized
 			w.Write([]byte("Unauthorized"))
 			return
 		}
 
+		// Get remote user ip address from request
 		userIp, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			log.Printf("Request Ip error: %v", err)
 		}
+		// Pass user info into current context
 		ctx = context.WithValue(ctx, "ip", &userIp)
 		ctx = context.WithValue(ctx, "userId", &userId)
 		ctx = context.WithValue(ctx, "is_authorized", isAuthorized)
@@ -80,17 +88,21 @@ func Authenticate(h http.Handler) http.Handler {
 	})
 }
 
+// validateBearerAuthHeader - validate incoming request Authorization header
 func validateBearerAuthHeader(ctx context.Context, r *http.Request) (*jwt.Token, error) {
 	var tokenString string
+	// Grab header values from Authorization key and split into [`Bearer` and `{token}`] slice
 	auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 	if len(auth) != 2 || auth[0] != "Bearer" {
 		return nil, errors.New(config.CredentialsError)
 	}
 	tokenString = auth[1]
+	// Is token valid?
 	token, err := ctx.Value("userService").(*services.UserServices).ValidateToken(&tokenString)
 	return token, err
 }
 
+// writeResponse - feed response result into the final request response
 func writeResponse(w http.ResponseWriter, response interface{}, code int) {
 	jsonResponse, _ := json.Marshal(response)
 	w.WriteHeader(code)
