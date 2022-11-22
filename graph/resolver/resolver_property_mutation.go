787040849a16b7e16ec8390fd0940a20d261302f
3dw1nM0535 <workockmoses@gmail.com>
2022-11-23 00:04:45 +0300

    feat(property): implement create property resolver

--- graph/resolver/resolver_property_mutation.go
+++ graph/resolver/resolver_property_mutation.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/3dw1nM0535/nyatta/config"
 	"github.com/3dw1nM0535/nyatta/graph/model"
+	"github.com/3dw1nM0535/nyatta/services"
 )
 
 // CreateProperty is the resolver for the createProperty field.
 func (r *mutationResolver) CreateProperty(ctx context.Context, input model.NewProperty) (*model.Property, error) {
-	panic(fmt.Errorf("not implemented: CreateProperty - createProperty"))
+	newProperty, err := ctx.Value("propertyService").(*services.PropertyServices).CreateProperty(&input)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", config.ResolverError, err)
+	}
+	return newProperty, nil
 }
