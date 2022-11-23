2be46e473ea4a0a4c18caa04885c299df265cdea
3dw1nM0535 <workockmoses@gmail.com>
2022-11-24 00:41:29 +0300

    chore(test): property testing

--- graph/resolver/resolver_property_test.go
+++ graph/resolver/resolver_property_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/3dw1nM0535/nyatta/graph/model"
 	"github.com/3dw1nM0535/nyatta/util"
+	"github.com/99designs/gqlgen/client"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -39,5 +40,21 @@ func Test_Property_Resolver(t *testing.T) {
 		assert.Equal(t, createProperty.CreateProperty.Name, "Oloolua Villas")
 		assert.Equal(t, createProperty.CreateProperty.PostalCode, "00208")
 		assert.Equal(t, createProperty.CreateProperty.Town, "Ngong Hills")
+	})
+
+	t.Run("should_get_property_details", func(t *testing.T) {
+		var getProperty struct {
+			GetProperty struct {
+				Name       string
+				PostalCode string
+			}
+		}
+
+		query := `query ($id: ID!) { getProperty(id: $id) { name, postalCode } }`
+
+		srv.MustPost(query, &getProperty, client.Var("id", createProperty.CreateProperty.ID))
+
+		assert.Equal(t, createProperty.CreateProperty.Name, getProperty.GetProperty.Name)
+		assert.Equal(t, createProperty.CreateProperty.PostalCode, getProperty.GetProperty.PostalCode)
 	})
 }
