21b05e23ef9f34448b50083927d1b07ee5684374
3dw1nM0535 <workockmoses@gmail.com>
2022-11-26 20:28:20 +0300

    chore(resolver): revert to default generated resolver file

--- graph/resolver/schema.resolvers.go
+++ graph/resolver/schema.resolvers.go
@@ -4,8 +4,55 @@ package resolver
 // will be copied through when generating and any unknown code will be moved to the end.
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/3dw1nM0535/nyatta/config"
 	"github.com/3dw1nM0535/nyatta/graph/generated"
+	"github.com/3dw1nM0535/nyatta/graph/model"
+	"github.com/3dw1nM0535/nyatta/services"
 )
+
+// SignIn is the resolver for the signIn field.
+func (r *mutationResolver) SignIn(ctx context.Context, input model.NewUser) (*model.Token, error) {
+	token, err := ctx.Value("userService").(*services.UserServices).SignIn(&input)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", config.ResolverError, err)
+	}
+	return &model.Token{Token: *token}, nil
+}
+
+// CreateProperty is the resolver for the createProperty field.
+func (r *mutationResolver) CreateProperty(ctx context.Context, input model.NewProperty) (*model.Property, error) {
+	newProperty, err := ctx.Value("propertyService").(*services.PropertyServices).CreateProperty(&input)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", config.ResolverError, err)
+	}
+	return newProperty, nil
+}
+
+// AddAmenity is the resolver for the addAmenity field.
+func (r *mutationResolver) AddAmenity(ctx context.Context, input model.AmenityInput) (*model.Amenity, error) {
+	panic(fmt.Errorf("not implemented: AddAmenity - addAmenity"))
+}
+
+// GetUser is the resolver for the getUser field.
+func (r *queryResolver) GetUser(ctx context.Context, id string) (*model.User, error) {
+	foundUser, err := ctx.Value("userService").(*services.UserServices).FindById(id)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", config.ResolverError, err)
+	}
+	return foundUser, nil
+}
+
+// GetProperty is the resolver for the getProperty field.
+func (r *queryResolver) GetProperty(ctx context.Context, id string) (*model.Property, error) {
+	foundProperty, err := ctx.Value("propertyService").(*services.PropertyServices).GetProperty(id)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %v", config.ResolverError, err)
+	}
+	return foundProperty, nil
+}
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
