6a5e0e629af33877caff8ddba1a871e8dd88e10c
3dw1nM0535 <workockmoses@gmail.com>
2022-12-27 00:14:40 +0300

    rfr(schema): type changes

--- database/dbConnect.go
+++ database/dbConnect.go
@@ -15,61 +15,61 @@ var (
 		`
 CREATE TABLE IF NOT EXISTS users (
   id BIGSERIAL PRIMARY KEY,
-  email text NOT NULL UNIQUE,
-  first_name text NOT NULL,
-  last_name text NOT NULL,
-	avatar text NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  email TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  avatar TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );`,
 		`
 CREATE TABLE IF NOT EXISTS properties (
   id BIGSERIAL PRIMARY KEY,
-  name varchar(100) NOT NULL,
-  town varchar(50) NOT NULL,
-  postal_code varchar(20) NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  created_by bigint NOT NULL REFERENCES users ON DELETE CASCADE
+  name VARCHAR(100) NOT NULL,
+  town VARCHAR(50) NOT NULL,
+  postal_code VARCHAR(20) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_by BIGINT NOT NULL REFERENCES users ON DELETE CASCADE
 );		`,
 		`
 CREATE TABLE IF NOT EXISTS amenities (
   id BIGSERIAL PRIMARY KEY,
-  name varchar(100) NOT NULL,
-  provider varchar(100) NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  property_id bigint NOT NULL REFERENCES properties ON DELETE CASCADE
+  name VARCHAR(100) NOT NULL,
+  provider VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  property_id BIGINT NOT NULL REFERENCES properties ON DELETE CASCADE
 );
 `,
 		`
 CREATE TABLE IF NOT EXISTS property_units (
   id BIGSERIAL PRIMARY KEY,
-  bathrooms integer NOT NULL,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  property_id bigint NOT NULL REFERENCES properties ON DELETE CASCADE
+  bathrooms INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  property_id BIGINT NOT NULL REFERENCES properties ON DELETE CASCADE
 );
 	`,
 		`
 CREATE TABLE IF NOT EXISTS tenants (
   id BIGSERIAL PRIMARY KEY,
-  start_date timestamp NOT NULL,
-  end_date timestamp,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  property_unit_id bigint NOT NULL REFERENCES property_units ON DELETE CASCADE
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  property_unit_id BIGINT NOT NULL REFERENCES property_units ON DELETE CASCADE
 );
 	`,
 		`
 CREATE TABLE IF NOT EXISTS bedrooms (
   id BIGSERIAL PRIMARY KEY,
-  bedroom_number integer NOT NULL,
-  en_suite boolean NOT NULL DEFAULT false,
-  master boolean NOT NULL DEFAULT false,
-  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  property_unit_id bigint NOT NULL REFERENCES property_units ON DELETE CASCADE
+  bedroom_number INTEGER NOT NULL,
+  en_suite BOOLEAN NOT NULL DEFAULT false,
+  master BOOLEAN NOT NULL DEFAULT false,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  property_unit_id BIGINT NOT NULL REFERENCES property_units ON DELETE CASCADE
 );
 	`,
 	}
