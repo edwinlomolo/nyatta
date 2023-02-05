ee3bb87d54220f544acb76e2d2426338040927d3
3dw1nM0535 <workockmoses@gmail.com>
2023-02-05 10:08:19 +0300

    Custom component styling starter

---
+++ client/src/lib/styles/theme/components/button.tsx
@@ -0,0 +1,6 @@
+import { defineStyleConfig } from '@chakra-ui/react'
+
+const Button = defineStyleConfig({
+})
+
+export default Button
--- client/src/lib/styles/theme/index.ts
+++ client/src/lib/styles/theme/index.ts
@@ -1,8 +1,14 @@
 import { extendTheme } from '@chakra-ui/react'
 
+// import { Button } from './components'
 import { fonts } from './fonts'
 
 export const theme = extendTheme({
   fonts,
+  /*
+  components: {
+    Button,
+  },
+  */
 })
 
