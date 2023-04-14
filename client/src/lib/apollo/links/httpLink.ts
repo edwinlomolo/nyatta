26191cf39749a9a8c5fbc9e75066737e8862c69c
3dw1nM0535 <workockmoses@gmail.com>
2023-04-15 01:12:27 +0300

    Base url fixes

--- client/src/lib/apollo/links/httpLink.ts
+++ client/src/lib/apollo/links/httpLink.ts
@@ -1,8 +1,7 @@
 import { HttpLink } from '@apollo/client'
-import { isPrototypeEnv } from '@helpers'
 
 const httpLink = new HttpLink({
-  uri: `${isPrototypeEnv() ? process.env.NEXT_PUBLIC_LOCAL_API : process.env.NEXT_PUBLIC_BASE_API}/api`,
+  uri: `${process.env.NEXT_PUBLIC_BASE_API}/api`,
 })
 
 export default httpLink
--- client/src/lib/helpers/index.ts
+++ client/src/lib/helpers/index.ts
@@ -1,9 +1,2 @@
 export const apiUrl = `${process.env.NEXT_PUBLIC_BASE_API}/handshake`
 
-export const isPrototypeEnv = (): boolean => {
-  if (process.env.NEXT_PUBLIC_ENV === 'development') {
-    return true
-  } else {
-    return false
-  }
-}
