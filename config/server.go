14a59fbbcb5be3217caae9bef7e8496b3bee9381
3dw1nM0535 <workockmoses@gmail.com>
2023-05-31 15:16:56 +0300

    nothing much

--- config/server.go
+++ config/server.go
@@ -2,6 +2,6 @@ package config
 
 // Server - server configurations
 type ServerConfig struct {
-	ServerPort string // server port
-	ServerEnv  string // server environment
+	ServerPort string
+	ServerEnv  string
 }
