023bdbfd404ea6dfec4efcc7310d3b2fd680487f
3dw1nM0535 <workockmoses@gmail.com>
2023-06-09 15:48:16 +0300

    chore: linter align

--- ui/web/src/lib/views/landlord/steps/units.tsx
+++ ui/web/src/lib/views/landlord/steps/units.tsx
@@ -28,16 +28,16 @@ const Units = () => {
       <VStack overflowY="auto" h="20vh" spacing={{ base: 4, md: 6 }}>
         {fields.map((_, unitIndex) => (
           <Box w="100%" gap={4} key={unitIndex} >
-          <FormControl isInvalid={Boolean(errors?.units)}>
-            <FormLabel>Unit name</FormLabel>
-            <Input
-              size="sm"
-              {...register(`units.${unitIndex}.name`)}
-              placeholder="Name/ID"
-            />
-            {((errors?.units) != null) && <FormErrorMessage>{`${errors?.units.message}`}</FormErrorMessage>}
-            <FormHelperText>How do you name your units?</FormHelperText>
-          </FormControl>
+            <FormControl isInvalid={Boolean(errors?.units)}>
+              <FormLabel>Unit name</FormLabel>
+              <Input
+                size="sm"
+                {...register(`units.${unitIndex}.name`)}
+                placeholder="Name/ID"
+              />
+              {((errors?.units) != null) && <FormErrorMessage>{`${errors?.units.message}`}</FormErrorMessage>}
+              <FormHelperText>How do you name your units?</FormHelperText>
+            </FormControl>
           </Box>
         ))}
       </VStack>
