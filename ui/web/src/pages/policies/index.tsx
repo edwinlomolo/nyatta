423b5fe2b5d39d211d9435ed89c1bdb424302768
3dw1nM0535 <workockmoses@gmail.com>
2023-06-18 17:57:54 +0300

    chore: tos&policies

--- ui/web/src/pages/policies/index.tsx
+++ ui/web/src/pages/policies/index.tsx
@@ -1,5 +1,44 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Container, SimpleGrid } from '@chakra-ui/react'
+import Link from 'next/link'
 
-const PrivacyPolicy = (): JSX.Element => <Box>Privacy Policy</Box>
+const PrivacyPolicy = (): JSX.Element => (
+  <Container mt={10} align="center">
+    <SimpleGrid columns={{sm: 1, md: 2}} spacingY={10}>
+      <Box>
+        <Box
+          mb={10}
+          border="1px green solid"
+          p={4}
+          borderRadius="md"
+          as={Link}
+          cursor="pointer"
+          target="_blank"
+          _hover={{
+            shadow: "md"
+          }}
+          href="https://hackmd.io/@qPtRW4tNR2KxOuqBVUBp5A/S1FipYnP3"
+        >
+          Privacy Policy
+        </Box>
+      </Box>
+      <Box>
+        <Box
+          border="1px green solid"
+          p={4}
+          borderRadius="md"
+          as={Link}
+          cursor="pointer"
+          target="_blank"
+          _hover={{
+            shadow: "md"
+          }}
+          href="https://hackmd.io/@qPtRW4tNR2KxOuqBVUBp5A/S13cD5hwn"
+        >
+          Terms of Service
+        </Box>
+      </Box>
+    </SimpleGrid>
+  </Container>
+)
 
 export default PrivacyPolicy
