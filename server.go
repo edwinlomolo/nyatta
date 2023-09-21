6a3fe9938f9b454985527487c718bc30b3744099
3dw1nM0535 <workockmoses@gmail.com>
2023-09-21 12:25:03 +0300

    chore(DISABLE): authenticate api call

--- server.go
+++ server.go
@@ -99,7 +99,7 @@ func main() {
 	logHandler := h.LoggingHandler{}
 	r.Handle("/", playground.Handler("GraphQL", "/api"))
 	r.Handle("/handshake", h.AddContext(ctx, logHandler.Logging(h.Handshake())))
-	r.Handle("/api", h.AddContext(ctx, logHandler.Logging(h.Authenticate(srv))))
+	r.Handle("/api", h.AddContext(ctx, logHandler.Logging(srv)))
 
 	s := &http.Server{
 		Addr:    fmt.Sprintf("0.0.0.0:%s", serverConfig.ServerPort),
