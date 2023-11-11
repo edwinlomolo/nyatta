275d0ac9ce756da13c54693514448c191de6ce59
3dw1nM0535 <workockmoses@gmail.com>
2023-11-11 23:10:35 +0300

    fix: verify paystack webhook call

--- handler/logging.go
+++ handler/logging.go
@@ -1,10 +1,8 @@
 package handler
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/3dw1nM0535/nyatta/services"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,25 +17,5 @@ func (l *LoggingHandler) Logging(h http.Handler) http.Handler {
 		log.Infof("%s %s %s %s", r.RemoteAddr, r.Method, r.URL, r.Proto)
 		// next
 		h.ServeHTTP(w, r)
-	})
-}
-
-func MpesaChargeCallback() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var paystackMpesaCallbackResponse *services.PaystackCallbackResponse
-		ctx := r.Context()
-		logger := ctx.Value("log").(*logrus.Logger)
-
-		if err := json.NewDecoder(r.Body).Decode(&paystackMpesaCallbackResponse); err != nil {
-			logger.Errorf("%s:%v", "PaystackMpesaChargeCallbackReadingBodyRequestError", err)
-		}
-
-		if err := ctx.Value("paystackService").(*services.PaystackServices).ReconcilePaystackMpesaCallback(*paystackMpesaCallbackResponse); err != nil {
-			logger.Errorf("%s:%v", "PaystackMpesaChargeCallbackError", err)
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-		return
 	})
 }
---
+++ handler/paystack_webhook.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/3dw1nM0535/nyatta/services"
+	"github.com/sirupsen/logrus"
+)
+
+func MpesaChargeCallback() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var paystackMpesaCallbackResponse *services.PaystackCallbackResponse
+		ctx := r.Context()
+		logger := ctx.Value("log").(*logrus.Logger)
+
+		paystackSignature := r.Header.Get("x-paystack-signature")
+		paystackSecretKey := os.Getenv("PAYSTACK_SECRET_KEY")
+
+		hash := hmac.New(sha512.New, []byte(paystackSecretKey))
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Errorf("%s:%v", "PaystackMpesaChargeCallbackReadingBodyRequestError", err)
+		}
+
+		hash.Write(body)
+		expectedMac := hex.EncodeToString(hash.Sum(nil))
+		if expectedMac != paystackSignature {
+			http.Error(w, "Invalid signature", http.StatusUnauthorized)
+		}
+
+		json.Unmarshal(body, &paystackMpesaCallbackResponse)
+
+		if err := ctx.Value("paystackService").(*services.PaystackServices).ReconcilePaystackMpesaCallback(*paystackMpesaCallbackResponse); err != nil {
+			logger.Errorf("%s:%v", "PaystackMpesaChargeCallbackReadingBodyRequestError", err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+		return
+	})
+}
